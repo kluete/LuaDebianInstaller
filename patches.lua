89147ea0f9260a4303494239bc8416d9102ed936
Peter <plaufenb@localhost.localdomain>
2016-04-27 02:48:25 +0200

    fixed syntax err

--- patches.lua
+++ patches.lua
@@ -29,17 +29,6 @@ patches_def = {
 	},
 },
 
---[[
-{ title = "APT: proxy",
-	{	op = "addlines",
-		path = "/etc/apt/apt.conf",
-		nmatch = 'Acquire::http::Proxy',
-		args =
-		{	'Acquire::http::Proxy "http://192.168.1.4:3142/";',
-		},
-	},
-},]]
-
 { title = "APT: no sources",
 	{	op = "gsublines",
 		path = "/etc/apt/sources.list",
@@ -60,22 +49,6 @@ patches_def = {
 		},
 	},
 },
-
---[[
-ignored on Jessie/systemd, instead use:
- systemctl enable tmp.mount
-]]
-{ title = "memory rcS",
-	{	op = "addlines",
-		path = "/etc/default/rcS",				-- should now be in /etc/default/tmpfs !
-		args = {"RAMRUN=YES", "RAMLOCK=YES", "TMPTIME=0"},
-		-- RAMSHM, RAMTMP ?
-	},
-},
---[[
-or add to /etc/fstab
- tmpfs	/tmp/tmpfs	tmpfs	nodev,nosuid,mode=1777,strictatime	0 0
-]]
 
 { title = "root .bashrc",
 	{	op = "addlines",
@@ -100,16 +73,14 @@ or add to /etc/fstab
 },
 
 { title = "user .profile DEV",
-	{	op = "gsublines",
+	{	op = "addlines",
 		path = "$LSK/.profile",
-		nmatch = 'export DEVELOPMENT',
+		nmatch = 'export LXDEV',
 		args =
-		{	'export DEVELOPMENT="$HOME/development"',
-			'export P4_WORKSPACE="$DEVELOPMENT/inhance_depot"',
+		{	'export LXDEV=/media/dev',
+			'export P4_WORKSPACE="$LXDEV/vlc_depot"',
 			'export WXCONFIG_LUA="$P4_WORKSPACE/DebLua/wxconfig.lua"',
-			'',
-			'export LXDEV="$HOME/development"',
-			'export LXBUILD="$HOME/development/build"',
+			'export LXBUILD="$LXDEV/build"',
 			'export LXGIT="$LXDEV/git"',
 			'',
 		},
@@ -174,140 +145,6 @@ EndSection
 
 }
 
---[=[
-
-{ title = "X11: qemu vostro430",
-	{
-		op = "addlines",
-		path = "/etc/X11/xorg.conf",
-		args =
-		{	"", [[
-Section "Monitor"
-  Identifier  "Monitor0"
-  HorizSync   20.0 - 50.0
-  VertRefresh 40.0 - 80.0
-EndSection
-
-Section "Device"
-  Identifier "Device0"
-  Driver     "vesa"
-EndSection
-
-Section "Screen"
-  Identifier   "Screen0"
-  Device       "Device0"
-  Monitor      "Monitor0"
-  DefaultDepth 24
-  Subsection "Display"
-    Depth 24
-#    Modes "1280x1024"
-     Modes "1440x900"
-#    Modes "1440x1024"
-  EndSubsection
-EndSection
-]]
-		},
-	},
-},
-
-]=]
-
---[[
-
-========================
-
- op = "addlines",
- path = "/etc/hosts"
- args = 
- {	"192.168.1.4	debnas",
- }
- 
-================================
-
- #Perforce
-export P4PORT=debnas:1666
-export P4USER=peterl
-
-
-{ title = "UFW: disable IPv6",
-	{	op = "gsublines",
-		path = "/etc/default/ufw",
-		args =
-		{	{'\nIPV6=yes\n', '\nIPV6=no\n'},
-		},
-	},
-		
-	{	op = "gsublines",
-		path = "/etc/ufw/sysctl.conf",
-		args =
-		{	{'(net/ipv6/conf/default/autoconf)=1', '%1=0'},
-			{'(net/ipv6/conf/all/autoconf=)1', '%10'},		-- try!
-		},
-	},
-},
-
-{ title = "SSH server: no DNS lookup",
-	{	op = "gsublines",
-		path = "/etc/ssh/sshd_config",
-		args =
-		{	{"(Protocol%s+2\n)", "%1UseDNS no\n"},			-- no DNS is CRITICAL to be able to work offline!
-			{"PermitRootLogin(.+)\n", " no"},
-			{"X11Forwarding(.+)\n", " no"},
-			{"#(IgnoreUserKnownHosts%s+yes\n)", "%1"},
-			-- PrintLastLog no
-		},
-	},
-},
-
-{ title = "network config 192.168.1.7",
-	{	op = "addlines",
-		path = "/etc/network/interfaces",
-		args =
-		{	"# This file describes the network interfaces available on your system",
-			"# and how to activate them. For more information, see interfaces(5).",
-			"# The loopback network interface",
-			"auto lo",
-			"iface lo inet loopback",
-			"",
-			"# The primary network interface",
-			"allow-hotplug eth0",
-			"iface eth0 inet static",
-			"	address 192.168.1.7",
-			"	netmask 255.255.255.0",
-			"	network 192.168.1.0",
-			"	broadcast 192.168.1.255",
-			"	gateway 192.168.1.1",
-			"	# dns-* options are implemented by the resolvconf package, if installed",
-			"	dns-nameservers 87.216.1.86 87.216.1.66",
-			-- {"	dns-search theloft.dj"},
-		},
-	},
-},
-
-{ title = "SSD fstab",
-	{	op = "addlines",
-		path = "/etc/fstab",
-		args =
-		{	"none	/tmp		tmpfs	defaults,noatime,mode=1777					0	0",
-			"none	/var/log	tmpfs	defaults,noatime,uid=0,gid=4,mode=2775		0	0",
-			"none	/var/spool	tmpfs	defaults,noatime,uid=0,gid=102,mode=2775	0	0",
-			"none	/var/local	tmpfs	defaults,noatime,uid=0,gid=50,mode=2775		0	0",
-			--		rsyslog needs
-			--			/var/spool/rsyslog	root:adm
-
-		},
-	},
-				
-	-- SSD TRIM
-	{	op = "gsublines",
-		path = "/etc/fstab",
-		args =
-		{	{"(%s/%s+ext4%s+(noatime,)?errors=remount-ro)", "%1,discard"},
-		},
-	},				
-},
-]]
-
 ---- Xfce4 Configuration Definitions -------------------------------------------
 
 xfce4_def =
@@ -354,31 +191,4 @@ xfce4_def =
 }
 
 xfce4_def_list = {"Appearance", "Panels", "keyboard", "Thunar view"}
-
---[[	
-	["test"] =
-	{
-		{"xfce4-desktop",		"/backdrop/screen0/monitor0/color1",		{"0u", "65535u", "0u", "65535u"}},
-	},
-
-	["reset"] =
-	{
-		{"xfce4-desktop",		"/backdrop/screen0/monitor0/color1",		{"2570u", "49884u", "45891u", "65535u"}},
-	},
-]]
-
-	--[[
-	/plugins/plugin-5                         clock
-	/plugins/plugin-5/digital-format          %a %d %b %Y   %H:%M:%S
-	/plugins/plugin-5/show-frame              false
-]]
-
---[[
-	on WHEEZY thunar icon view settings are in
-		~/.config/Thunar/thunarrc
-			DefaultView=ThunarDetailsView
-
-	on JESSIE thunar icon view settings are in
-		~/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml
-]]
 
